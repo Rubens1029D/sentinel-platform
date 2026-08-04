690682832039ea6d40a69a5b281e485eadb70ab0
Rubens Garcia <224635738+Rubens1029D@users.noreply.github.com>
2026-08-03 18:00:15 -0600

    feat(api): add configuration, validation and swagger

--- apps/platform-api/src/app.module.ts
+++ apps/platform-api/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['apps/platform-api/.env', '.env'],
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'test', 'production')
+          .default('development'),
+        PORT: Joi.number().port().default(3000),
+        API_PREFIX: Joi.string().default('api/v1'),
+      }),
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: false,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
